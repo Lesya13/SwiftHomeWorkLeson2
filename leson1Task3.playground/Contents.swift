f712c03623389833911d6631c894fcefe6a84e7c
Lesya <pupochechka@mail.ru>
2019-09-10 19:45:50 +0300

    ДЗ 3 к первому уроку

---
+++ leson1Task3.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var summa: Double = 100
+var percent: Double = 10
+summa = summa + (summa * percent / 100);
+summa += (summa * percent / 100);
+summa += (summa * percent / 100);
+summa += (summa * percent / 100);
+summa += (summa * percent / 100);
+
+ print("Через 5 лет вклад будет таким = \(summa)")
